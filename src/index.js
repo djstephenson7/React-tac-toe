1bfb531b675988abe8423d75c8aef2484fc3072a
djstephenson7 <dan_0502@hotmail.co.uk>
2018-11-30 11:57:45 +0000

    Functioning Tic-tac-toe game

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import './index.css';
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
-    {props.value}
+      {props.value}
     </button>
-  )
+  );
 }
 
 class Board extends React.Component {
@@ -21,21 +21,34 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
-      squares :squares,
-      xIsNext: !this.state.isIsNext,
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
     });
   }
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]}
-    onClick={() => this.handleClick(i)}
-    />
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div>
         <div className="status">{status}</div>
@@ -79,3 +92,23 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
